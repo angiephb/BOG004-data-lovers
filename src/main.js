837a72ee885ae9d9dd95edd1e71826f6d6226f45
angiephb <angie.pio30@gmail.com>
2022-02-07 17:20:48 -0500

    cambios en css

--- src/main.js
+++ src/main.js
@@ -3,21 +3,5 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-const prueba = (432);
-
-const btn= document.getElementById("btn1");
-btn.addEventListener("click", open1 =>{
- document.getElementById("pag2").style.display="block";
- document.getElementById("pag1").style.display="none";
-})
-
-let msg= document.getElementById("msg");
-msg.innerHTML= " ";
-
-let get= document.getElementById("btn2");
-get.addEventListener("click", open2=>{
-const nameuser= document.getElementById("name").value;
- msg.innerHTML = nameuser + ' Aquí podras ver una pelicula a tu gusto y conocer algunas curiosidades ';
-})
 
 console.log(example, data);
