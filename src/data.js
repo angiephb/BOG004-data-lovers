feb1f7015f1ca9b7c3faef8a38e4f03e2747e32e
gilliam24 <gilliamargelmonterroza@gmail.com>
2022-02-13 16:12:19 -0500

    subiendo actualizaciones

--- src/data.js
+++ src/data.js
@@ -1,9 +1,37 @@
-// estas funciones son de ejemplo
+import data from './data/ghibli/ghibli.js';// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+//export default crear
+//listf.innerHTML='<ul>${template}</ul>';
+ 
+
+//export const anotherExample = () => {
+  //return 'OMG';
+//}*/
+
+const filtervehiculos =() =>{
+  let vehicles2= data.films.filter((films)=> {
+  return films.vehicles;
+  })
+
+  .map((films) => {
+    return `${films.vehicles} ${films.title}`;
+  });
+  console.log("vehiculos", vehicles2); 
+}
+
+/*const filtropelis =() =>{
+  const vehicles = data.films.filter((films) => films.vehicles = `${ films.title } ${ - films.vehicles }`);
+  console.log( vehicles );
+ /* let filtervehiculos= document.getElementById("vehicles");
+  filtervehiculos.innerHTML=vehicles.join('');
+ return filtropelis;*/
+
+
+export default filtervehiculos;
+/*const crear =() =>{
+  const template = data.films.map((films) => `<li>${films.title } ${ - films.release_date}</li>`);
+  console.log( template );
+  let list=document.getElementById("list");
+  list.innerHTML = template.join('')*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import filtervehiculos from './data.js';
+ //import data from './data/lol/lol.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 document.getElementById("pag2").style.display="none";
@@ -19,5 +19,22 @@ get.addEventListener("click", open2=>{
 const nameuser= document.getElementById("name").value;
  msg.innerHTML = nameuser + ' Aquí podras ver una pelicula a tu gusto y conocer algunas curiosidades ';
 })
+const crear =() =>{
+    const template = data.films.map((films) => `<li>${films.title } ${ - films.release_date}</li>`);
+    console.log( template );
+    let list=document.getElementById("list");
+    list.innerHTML = template.join('')
+  
+  }
+  
+document.getElementById("pag3").style.display="none";
 
-console.log(example, data);
+const btn1= document.getElementById("btn3");
+btn1.addEventListener("click", open3 =>{
+ document.getElementById("pag3").style.display="block";
+ document.getElementById("pag2").style.display="none";
+ document.getElementById("pag1").style.display="none";
+})
+console.log(data);
+crear();
+filtervehiculos();
